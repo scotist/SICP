7a414bf1ecff0af5f0cc91708d0b0012f231545b
Michael Sullivan <sapientia.veritas@gmail.com>
2016-05-04 15:43:46 -0700

    made fixture and tests for sum of squares

--- src/chapter1/test_ch1_3.py
+++ src/chapter1/test_ch1_3.py
@@ -6,6 +6,9 @@ from ch1_3 import square, sum_of_squares
 SQUARES = [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25),
            (-1, 1), (-2, 4), (-3, 9)]
 
+SQUARE_SUMS = [(0, 0, 0), (1, 1, 2), (1, 2, 5), (2, 3, 13),
+               (-1, -2, 5), (5, 6, 61)]
+
 
 @pytest.mark.parametrize('num', SQUARES)
 def test_squares(num):
@@ -13,4 +16,7 @@ def test_squares(num):
     assert square(num[0]) == num[1]
 
 
-
+@pytest.mark.parametrize('nums', SQUARE_SUMS)
+def test_sum_of_squares(nums):
+    """Test sum of squares function on a series of inputs."""
+    assert sum_of_squares(nums[0], nums[1]) == nums[2]
