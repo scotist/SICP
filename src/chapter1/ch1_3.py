6729a2a84878c55ac02db031cda49f38d708f6fe
Michael Sullivan <sapientia.veritas@gmail.com>
2016-05-04 15:13:32 -0700

    renamed all files, set up test file for 1_3

---
+++ src/chapter1/ch1_3.py
@@ -0,0 +1,11 @@
+"""Sum of squares of two largest inputs."""
+
+
+def square(n):
+    """Given a number, return its square."""
+    return n * n
+
+
+def sum_of_squares(x, y):
+    """Return sum of squares of parameters."""
+    return square(x) + square(y)
---
+++ src/chapter1/test_ch1_3.py
@@ -0,0 +1,8 @@
+"""Test squares and sum of squares module."""
+import pytest
+from ch1_3 import square, sum_of_squares
+
+
+def test_square_1():
+    """Test square on zero."""
+    assert 1 == 1
