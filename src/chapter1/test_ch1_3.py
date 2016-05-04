86ea1c28ecfd4e66d1fa519407af4dd87982f389
Michael Sullivan <sapientia.veritas@gmail.com>
2016-05-04 15:35:31 -0700

    made fixture, tests passing for square

--- src/chapter1/test_ch1_3.py
+++ src/chapter1/test_ch1_3.py
@@ -3,6 +3,14 @@ import pytest
 from ch1_3 import square, sum_of_squares
 
 
-def test_square_1():
-    """Test square on zero."""
-    assert 1 == 1
+SQUARES = [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25),
+           (-1, 1), (-2, 4), (-3, 9)]
+
+
+@pytest.mark.parametrize('num', SQUARES)
+def test_squares(num):
+    """Test squares function on a series of integers."""
+    assert square(num[0]) == num[1]
+
+
+
