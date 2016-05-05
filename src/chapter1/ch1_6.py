dd063a83a5cbf52af7b5ee925d5d9de36a1f2bfe
Michael Sullivan <sapientia.veritas@gmail.com>
2016-05-04 23:06:56 -0700

    made recursive square root function in python

---
+++ src/chapter1/ch1_6.py
@@ -0,0 +1,27 @@
+"""Approximate square root of input number."""
+from ch1_3 import square
+
+
+def average(x, y):
+    """Find average of two positive integers."""
+    return (x + y) / 2
+
+
+def improve(guess, num):
+    """Apply Heron's algorithm to improve square root guess."""
+    return average(guess, (num / guess))
+
+
+def good_enough(guess, num):
+    """Determine whether approximation is good enough."""
+    if abs(num - square(guess)) < 0.001:
+        return 'hello'
+    return 'goodbye'
+
+
+def sqrt_iter(guess, num):
+    """Iteratively approximate square root of desired number."""
+    if good_enough(guess, num) == 'hello':
+        return guess
+    else:
+        return sqrt_iter(improve(guess, num), num)
