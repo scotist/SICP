34a0af56eca727fa1a20146bbbfcb9029c842d0e
Michael Sullivan <sapientia.veritas@gmail.com>
2016-05-04 21:56:27 -0700

    added square root function in scheme

---
+++ src/chapter1/ch1_6.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (square x) (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+; sqrt-iter defined with new-if instead of with plain if causes an infinite loop, because always evaluates both of the arguments before applying the function to them. It calls the function recursively on both the conditional's consequent and its alternative, rather than evaluating the condition and applying either the consequent or the alternative.
