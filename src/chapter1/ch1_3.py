ba72ab2db10e49d3ca8fda384d1d7aec13aaf567
Michael Sullivan <sapientia.veritas@gmail.com>
2016-05-04 17:28:38 -0700

    cleanup file structure, fixed but in square of largest function, tests

--- src/chapter1/ch1_3.py
+++ src/chapter1/ch1_3.py
@@ -9,3 +9,13 @@ def square(n):
 def sum_of_squares(x, y):
     """Return sum of squares of parameters."""
     return square(x) + square(y)
+
+
+def square_of_largest(x, y, z):
+    """Return the sum of the squares of the larger two of three numbers."""
+    if x <= y and x <= z:
+        return sum_of_squares(y, z)
+    elif y <= x and y <= z:
+        return sum_of_squares(x, z)
+    elif z <= x and z <= y:
+        return sum_of_squares(x, y)
--- src/chapter1/test_ch1_3.py
+++ src/chapter1/test_ch1_3.py
@@ -1,6 +1,6 @@
 """Test squares and sum of squares module."""
 import pytest
-from ch1_3 import square, sum_of_squares
+from ch1_3 import square, sum_of_squares, square_of_largest
 
 
 SQUARES = [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25),
@@ -8,6 +8,10 @@ SQUARES = [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25),
 
 SQUARE_SUMS = [(0, 0, 0), (1, 1, 2), (1, 2, 5), (2, 3, 13),
                (-1, -2, 5), (5, 6, 61)]
+
+LARGER_SQUARE_SUMS = [(0, 0, 0, 0), (0, 0, 1, 1), (0, 1, 1, 2), (1, 2, 3, 13),
+                      (2, 3, 4, 25), (-2, -1, 5, 26), (2, -14, 0, 4),
+                      (4, 3, 1, 25), (2, 5, 3, 34), (-3, 15, 2, 229)]
 
 
 @pytest.mark.parametrize('num', SQUARES)
@@ -20,3 +24,9 @@ def test_squares(num):
 def test_sum_of_squares(nums):
     """Test sum of squares function on a series of inputs."""
     assert sum_of_squares(nums[0], nums[1]) == nums[2]
+
+
+@pytest.mark.parametrize('nums', LARGER_SQUARE_SUMS)
+def test_square_of_largest(nums):
+    """Test square of largest function on a series of inputs."""
+    assert square_of_largest(nums[0], nums[1], nums[2]) == nums[3]
